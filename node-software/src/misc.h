b9118ce4b312b7059c0c4c1d2791a1a45104b306
Sam Nicholson <sdn501@york.ac.uk>
2015-03-11 21:06:18 +0000

    NODE: Made some macros a bit better with brackets courtesy CCL's interviewers

--- node-software/src/misc.h
+++ node-software/src/misc.h
@@ -6,11 +6,11 @@
 #ifndef MISC_H_
 #define MISC_H_
 
-#define CORE_CLOCK_FREQ 21000000
-#define TIMER_PRESCALER 2
+#define CORE_CLOCK_FREQ (21000000)
+#define TIMER_PRESCALER (2)
 
 // Given a prescaler value (y) and a desired time in ms (x), calculate timer ticks
-#define get_ticks_from_ms(x, y)  (x * CORE_CLOCK_FREQ)/(1000 * y)
+#define get_ticks_from_ms(x, y)  ((x) * CORE_CLOCK_FREQ)/(1000 * (y))
 
 
 #endif /* MISC_H_ */
