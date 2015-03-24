c5e3cdd39650fe6f25fdf4ae30d6c7c022ba5cb1
Sam Nicholson <sdn501@york.ac.uk>
2015-03-24 17:01:53 +0000

    BASE: Wrote output stuff to display radio data on console

--- basestation-software/src/main.c
+++ basestation-software/src/main.c
@@ -18,24 +18,44 @@
 #define NODE_ADDR 0xFF
 
 void got_packet_data(uint16_t bytes);
+void output_byte_as_text(uint8_t byte);
+
+void output_byte_as_text(uint8_t byte)
+{
+    uint8_t print[] = "0xXX";
+    print[2] = (byte & 0xF0) >> 4;
+    print[2] = print[2] <= 9 ? '0' + print[2] : 'A' - 10 + print[2];
+
+    print[3] = byte & 0x0F;
+    print[3] = print[3] <= 9 ? '0' + print[3] : 'A' - 10 + print[3];
+
+    serial_print((char *)print);
+}
 
 void got_packet_data(uint16_t bytes)
 {
-    uint8_t data[5] = {0};
-    uint8_t send_data[] = "Hey";
+    uint8_t data[80];
 
-    uint16_t bytes_read = radio_retrieve_data(data, 4);
+    uint16_t bytes_read = radio_retrieve_data(data, 80);
 
-    serial_print("\r\nGot some radio data. Data was: ");
-    serial_print(data);
+    serial_print("\r\nGot some radio data. Count: ");
+    output_byte_as_text(data[1]);
+    serial_print(" of ");
+    output_byte_as_text(data[2]);
     serial_print("\r\n");
 
-    if (data[1] == 'H' && data[2] == 'i')
+    for (uint16_t i = 3; i < bytes_read; i += 4)
     {
-        radio_send_data(send_data, 3, data[0]);
+        output_byte_as_text(data[i]);
+        serial_print(", ");
+        output_byte_as_text(data[i + 1]);
+        serial_print(", ");
+        output_byte_as_text(data[i + 2]);
+        serial_print(", ");
+        output_byte_as_text(data[i + 3]);
+        serial_print("\r\n");
     }
-
-    serial_print("Sent a response \r\n");
+    serial_print("Data done. \r\n");
 }
 
 /**
@@ -71,7 +91,7 @@ void main(void)
     // Set up for receive
     serial_print("Sending test packet, waiting for RX\r\n");
     radio_powerstate(true);
-    radio_send_data("Hi", 2, 0x01);
+    radio_send_data((uint8_t*)"Hi", 2, 0x01);
     radio_receive_activate(true);
 
     serial_print("Startup done. Sleeping\r\n");
